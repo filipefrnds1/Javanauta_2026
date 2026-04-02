2ae1ee6aae1b343e1b3361d2a37c3adae4cb9802
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-02 17:28:41 -0300

    Javanauta Switch Case

---
+++ SwitchCase.java
@@ -0,0 +1,46 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class SwitchCase {
+
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        int diaDaSemana = sc.nextInt();
+
+        switch (diaDaSemana){
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("inválido");
+                break;
+
+
+        }
+        sc.close();
+
+    }
+}
