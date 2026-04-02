51b6e3fa76f64b904ca4ab7306d3fdf20f9fcbf6
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-02 15:57:19 -0300

    Javanauta Operador Ternário

---
+++ OperadorTernario.java
@@ -0,0 +1,14 @@
+public class OperadorTernario {
+
+    public static void main(String[] args) {
+
+        boolean temDinheiro = true;
+        //boolean temCartao = false;
+
+        String mensagem = (temDinheiro) ? "Pede um ifood" : "Come o que tem em casa"; // expressão ternária
+
+        System.out.println(mensagem);
+
+
+    }
+}
